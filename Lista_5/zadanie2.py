0b8b00f4e6a0a15322119eeb899104d7bbb49337
TypowyJugol <wojtek.roman254@gmail.com>
2020-12-16 14:00:34 +0100

    l5

--- Lista_5/zadanie2.py
+++ Lista_5/zadanie2.py
@@ -1,8 +1,8 @@
 from scipy.interpolate import lagrange
 from numpy.polynomial.polynomial import Polynomial
 
-x = [0,3,6]
-y = [1.225, 0.905, 0.652]
+x = [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3]
+y = [-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334]
 
 poly = lagrange(x, y)
 
