614c0867ff6f1aff1908ba836562ba51341c5d25
TypowyJugol <wojtek.roman254@gmail.com>
2020-12-25 17:31:21 +0100

    l5

--- Lista_5/zadanie2.py
+++ Lista_5/zadanie2.py
@@ -1,9 +1,15 @@
 from scipy.interpolate import lagrange
-from numpy.polynomial.polynomial import Polynomial
 
-x = [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3]
-y = [-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334]
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [1,1.25,1.5,1.75,2,2.25,2.5,2.75,3]
+
+y = [-0.5403,-0.0104,0.9423,1.7445,1.3073,-0.7718,-2.4986,-0.7903,2.7334]
+
 
 poly = lagrange(x, y)
+newx = np.linspace(0.8,3.2,1000)
 
-print(Polynomial(poly).coef)
+plt.plot(newx,poly(newx))
+plt.show()
